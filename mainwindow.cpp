8ba462e178b1de1e3e9d74e056d17f19d21f56c1
Danil Nikiforov <57360250+Crusher-xxx@users.noreply.github.com>
2024-09-01 23:09:38 +0300

    initialize project

---
+++ mainwindow.cpp
@@ -0,0 +1,9 @@
+#include "mainwindow.hpp"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    ui.setupUi(this);
+}
+
+MainWindow::~MainWindow() {}
---
+++ mainwindow.hpp
@@ -0,0 +1,19 @@
+#ifndef MAINWINDOW_HPP
+#define MAINWINDOW_HPP
+
+#include "./ui_mainwindow.h"
+
+#include <QMainWindow>
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow ui;
+};
+#endif // MAINWINDOW_HPP
